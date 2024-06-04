295a88b04b59e11cf7733414c85d076e79d2ed27
Naman Soni <namantsoni@gmail.com>
2024-06-04 19:58:10 +0530

    Database & Tailwind CSS setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,13 @@
 import express, {Request, Response} from "express";
+import mongoose from "mongoose";
 import cors from "cors";
 import "dotenv/config";
 
+mongoose.connect(process.env.MONGO_CONNECTION_STRING as string).then(() => {
+    console.log('Database Connected!')
+}).catch((err) => {
+    console.log(err);
+})
 const app = express();
 
 app.use(express.json());
