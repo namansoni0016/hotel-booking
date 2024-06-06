b4c6c44a9bbe33cd3f0083579e4275dd6b841182
Naman Soni <namantsoni@gmail.com>
2024-06-06 11:49:41 +0530

    Header, Hero & Footer Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
-function App() {
+import { BrowserRouter as Router, 
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Layout from "./layouts/Layout";
 
+function App() {
   return (
-    <>
-      <h1>Welcome!</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout>
+          <p>Home Page</p>
+        </Layout>}/>
+        <Route path="/search" element={<Layout>
+          <p>Search Page</p>
+        </Layout>}/>
+        <Route path="*" element={<Navigate to="/"/>}/>
+      </Routes>
+    </Router>
   )
 }
 
