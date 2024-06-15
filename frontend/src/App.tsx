866a8e1c832ba9705da43a58640438d79e350bd0
Naman Soni <namantsoni@gmail.com>
2024-06-15 17:34:04 +0530

    Register Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,15 @@ import { BrowserRouter as Router,
   Navigate,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout>
-          <p>Home Page</p>
-        </Layout>}/>
-        <Route path="/search" element={<Layout>
-          <p>Search Page</p>
-        </Layout>}/>
+        <Route path="/" element={<Layout><p>Home Page</p></Layout>}/>
+        <Route path="/search" element={<Layout><p>Search Page</p></Layout>}/>
+        <Route path="/register" element={<Layout><Register/></Layout>}/>
         <Route path="*" element={<Navigate to="/"/>}/>
       </Routes>
     </Router>
