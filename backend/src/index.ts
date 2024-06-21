821c951f3af0ceb786d03ed2754acb56ccdbc414
Naman Soni <namantsoni@gmail.com>
2024-06-21 09:42:12 +0530

    User login state

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import "dotenv/config";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
+import cookieParser from "cookie-parser";
 
 mongoose.connect(process.env.MONGO_CONNECTION_STRING as string).then(() => {
     console.log('Database Connected!')
@@ -12,9 +13,13 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING as string).then(() => {
 })
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
 
 //Routes
 app.use("/api/auth", authRoutes);
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,6 +3,7 @@ import { check, validationResult } from "express-validator";
 import User from "../models/user";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import verifyToken from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
@@ -36,6 +37,10 @@ router.post("/login", [check("email", "Email is Required!").isEmail(),
         console.log(error);
         res.status(500).json({ message: "Something went wrong!"});
     }
+});
+
+router.get("/validate-token", verifyToken, (req: Request, res: Response) => {
+    res.status(200).send({userId: req.userId})
 })
 
 export default router;
